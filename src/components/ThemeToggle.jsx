3d2bb0390dddf0b301aaaf6c1bd78a4be6ccf2e7
Juyoung <juyu9407@gmail.com>
2025-08-03 17:06:10 -0600

    feat: add AboutSection with responsive layout and content

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -34,7 +34,7 @@ export const ThemeToggle = () => {
       className={cn(
         // "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
         // "focus:outline-hidden"
-        "p-1 rounded-full transition-colors duration-300 focus:outline-none"
+        "p-1 rounded-full transition-colors duration-300 focus:outline-none cursor-pointer"
       )}
     >
       {isDarkMode ? (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 // import { ThemeToggle } from "@/components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { Navbar } from "@/components/Navbar";
-import { HeroSection } from "../components/HeroSection";
+import { HeroSection } from "@/components/HeroSection";
+import { AboutSection } from "@/components/AboutSection";
 
 export const Home = () => {
   return (
@@ -18,6 +19,7 @@ export const Home = () => {
       {/* Main Content */}
       <main>
         <HeroSection />
+        <AboutSection />
       </main>
       {/* Footer */}
     </div>
